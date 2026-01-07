15caf1da742b9338d19b8a1ce5128ddeac9e9edf
Dhruv Shah <dhruvkshah75@gmail.com>
2026-01-07 20:00:37 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# VectorMesh - Distributed RAG Ingestion Engine
---
+++ services/gateway/internal/storage/minio.go
@@ -0,0 +1,11 @@
+package storage
+
+import(
+	"github.com/minio/minio-go/v7"
+	"github.com/minio/minio-go/v7/pkg/credentials"
+)
+
+// InitMinio establishes connection to the MinIO Server 
+func InitMinio() *minio.Client {
+
+}
