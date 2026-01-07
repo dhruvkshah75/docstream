2a10c1ae2ca2ad558254d5250dddd66c42e823e6
Dhruv Shah <dhruvkshah75@gmail.com>
2026-01-07 23:27:59 +0530

    added publish job method in the queue of rabbitmq

--- services/gateway/internal/producer/rabbitmq.go
+++ services/gateway/internal/producer/rabbitmq.go
@@ -41,3 +41,16 @@ func InitRabbitMQ() (*amqp.Connection, *amqp.Channel, amqp.Queue){
 	log.Println("Successfully connected to RabbitMQ")
 	return conn, ch, q
 }
+
+// PublishJob sends a JSON payload to the queue
+func PublishJob(ch *amqp.Channel, q amqp.Queue, body []byte) error {
+	return ch.Publish(
+		"",     // exchange
+		q.Name, // routing key
+		false,  // mandatory
+		false,  // immediate
+		amqp.Publishing{
+			ContentType: "application/json",
+			Body:        body,
+		})
+}
